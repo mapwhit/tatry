c02227a4fc58d9dd3f01d1c190486db20a0a4d50
Damian Krzeminski <pirxpilot@furkot.com>
2019-02-04 17:56:30 -0700

    disable empty test

--- test/tatry.js
+++ test/tatry.js
@@ -1,9 +1,7 @@
-var should = require('should');
-var tatry = require('../');
+const tatry = require('../');
 
 describe('tatry node module', function () {
   it('must have at least one test', function () {
     tatry();
-    should.fail('Need to write tests.');
   });
 });

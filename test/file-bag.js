12ad97fad64185b4b1989ee9c4cf93c73f9eca18
Damian Krzeminski <pirxpilot@furkot.com>
2019-02-05 18:18:51 -0700

    fix tests for file-bag

--- test/file-bag.js
+++ test/file-bag.js
@@ -1,15 +1,20 @@
 const should = require('should');
 const fileBag = require('../lib/file-bag');
 
-const data = require('/var/lib/tatry/summary.json');
+const data = require('./fixtures/data-with-meta/tatry.json');
 
 describe('file bag', function () {
   it('find valid point', function () {
 
     const fb = fileBag(data);
 
-    const file = fb.find([ -106.827126, 40.483468 ]);
+    const { file, meta } = fb.find([ -106.827126, 40.483468 ]);
     should.exist(file);
-    file.should.eql('data/SRTM_W_250m_4_3.tif');
+    file.should.eql('/var/lib/tatry/srmt-250m_13_3.tif');
+
+    should.exist(meta);
+    meta.should.have.property('width', 480);
+    meta.should.have.property('height', 192);
+    meta.should.have.property('it').with.length(6);
   });
 });

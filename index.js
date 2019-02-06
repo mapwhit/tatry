90e8977e6d12fd4f4ea9ad2178fd9707b93460e6
Damian Krzeminski <pirxpilot@furkot.com>
2019-02-05 20:17:45 -0700

    add express based API server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./lib/tatry');
+require('dotenv').config({ path: '/etc/default/tatry' });
+
+const express = require('express');
+
+const TATRY_PORT = process.env.TATRY_PORT || 3080;
+const TATRY_DATA_PATH = process.env.TATRY_DATA_PATH || '/var/lib/tatry';
+
+const app = express();
+
+const { v1 } = require('./lib/tatry')(TATRY_DATA_PATH);
+
+app.use('/api/v1', v1);
+
+module.exports = app;
+
+if (!module.parent) {
+  app.listen(TATRY_PORT);
+  console.log('Listening on port', TATRY_PORT);
+}
--- test/tatry.js
+++ test/tatry.js
@@ -1,7 +1,56 @@
-const tatry = require('../');
+process.env.TATRY_DATA_PATH = __dirname + '/fixtures/data';
 
-describe('tatry node module', function () {
-  it('must have at least one test', function () {
-    tatry();
+const request = require('supertest');
+
+const app = require('../');
+
+describe('tatry', function () {
+
+  it('responds to get', function () {
+    return request(app)
+      .get('/api/v1/lookup')
+      .query({ locations: '40.483468,-106.827126|40.5,-106.1|40.8,-106.9' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => response.body.should.have.property('results', [
+        { latitude: 40.483468, longitude: -106.827126, elevation: 2068 },
+        { latitude: 40.5, longitude: -106.1, elevation: 3045 },
+        { latitude: 40.8, longitude: -106.9, elevation: 2512 }
+      ]));
+  });
+
+  it('responds to post', function () {
+    return request(app)
+      .post('/api/v1/lookup')
+      .send({ locations: [
+        { latitude: 40.483468, longitude: -106.827126 },
+        { latitude: 40.5, longitude: -106.1 },
+        { latitude: 40.8, longitude: -106.9 }
+      ]})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => response.body.should.have.property('results', [
+        { latitude: 40.483468, longitude: -106.827126, elevation: 2068 },
+        { latitude: 40.5, longitude: -106.1, elevation: 3045 },
+        { latitude: 40.8, longitude: -106.9, elevation: 2512 }
+      ]));
+  });
+
+  it('responds to get with invalid coordinates', function () {
+    return request(app)
+      .get('/api/v1/lookup')
+      .query({ locations: '10,10' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => response.body.should.eql({
+        results: [{
+          longitude: 10,
+          latitude: 10,
+          elevation: -32768
+        }]
+      }));
   });
 });

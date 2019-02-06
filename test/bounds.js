0e197108c70cbc8e10d354940e402c52094d7097
Damian Krzeminski <pirxpilot@furkot.com>
2019-02-05 18:24:48 -0700

    fix tests for bounds

--- test/bounds.js
+++ test/bounds.js
@@ -1,30 +1,34 @@
 const should = require('should');
+const path = require('path');
+
 const bounds = require('../lib/bounds');
 
 describe('bounds', function () {
   it('should extract bounds for file', function () {
+    const file = path.resolve(__dirname, 'fixtures', 'data', 'srmt-250m_13_3.tif');
 
-    return bounds('/var/lib/tatry/SRTM_W_250m_4_3.tif')
+    return bounds(file)
       .then(bounds => {
         should.exist(bounds);
         bounds.should.eql({
-            minX: -120.00208329493002,
-            maxX: -104.99999998560001,
-            minY: 36.00624984639,
-            maxY: 42.00833317012,
-            file: '/var/lib/tatry/SRTM_W_250m_4_3.tif',
-            meta: {
-              width: 7201,
-              height: 2881,
-              it: [
-                57601.000073728006,
-                480.00000076799995,
-                0,
-                20163.999953919996,
-                0,
-                -480.00000076799995
-              ]
-            }
+          minX: -107.00208331573002,
+          maxX: -106.00208331733002,
+          minY: 40.408333172679995,
+          maxY: 40.808333172039994,
+          file,
+          meta: {
+            width: 480,
+            height: 192,
+            it: [
+              51361.000073728006,
+              480.00000076799995,
+              0,
+              19587.999953919996,
+              0,
+              -480.00000076799995
+            ]
+          }
+
         });
       });
   });

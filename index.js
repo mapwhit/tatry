a3cc23a80717172e0bb0f06b05af6e4413720946
Damian Krzeminski <pirxpilot@furkot.com>
2019-02-07 08:41:29 -0700

    enable server-timings headers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: '/etc/default/tatry' });
 
 const express = require('express');
+const timings = require('server-timings');
 
 const TATRY_PORT = process.env.TATRY_PORT || 3080;
 const TATRY_DATA_PATH = process.env.TATRY_DATA_PATH || '/var/lib/tatry';
@@ -10,6 +11,8 @@ const app = express();
 app.enable('trust proxy');
 app.disable('x-powered-by');
 app.set('query parser', 'simple');
+
+app.use(timings);
 
 const { v1 } = require('./lib/tatry')(TATRY_DATA_PATH);
 app.use('/api/v1', v1);

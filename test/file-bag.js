4a86b7f4af37f6aaaa18b1952bfa989f3e3fc4ba
Damian Krzeminski <pirxpilot@furkot.com>
2019-02-10 20:25:58 -0700

    select best resolution data
    if multiple files cover the same area select the one that has best
    resolution

--- test/file-bag.js
+++ test/file-bag.js
@@ -1,11 +1,12 @@
 const should = require('should');
 const fileBag = require('../lib/file-bag');
 
-const data = require('./fixtures/data-with-meta/tatry.json');
 
 describe('file bag', function () {
-  it('find valid point', function () {
 
+  it('find file for valid point', function () {
+
+    const data = require('./fixtures/data-with-meta/tatry.json');
     const fb = fileBag(data);
 
     const { file, meta } = fb.find([ -106.827126, 40.483468 ]);
@@ -17,4 +18,20 @@ describe('file bag', function () {
     meta.should.have.property('height', 192);
     meta.should.have.property('it').with.length(6);
   });
+
+  it('find file with best resolution', function () {
+
+    const data = require('./fixtures/data-with-meta/tatry-resolution.json');
+    const fb = fileBag(data);
+
+    const { file, meta } = fb.find([ -106.827126, 40.483468 ]);
+    should.exist(file);
+    file.should.eql('/var/lib/tatry/srmt-60m_13_3.tif');
+
+    should.exist(meta);
+    meta.should.have.property('width', 480);
+    meta.should.have.property('height', 192);
+    meta.should.have.property('it').with.length(6);
+  });
+
 });

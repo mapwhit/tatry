2a9150a0c9d4c2bf387d84df21d7af39b1f7ecb9
Damian Krzeminski <pirxpilot@furkot.com>
2019-03-15 07:53:31 -0600

    replace express with connect and router

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 require('dotenv').config({ path: '/etc/default/tatry' });
 
-const express = require('express');
+const connect = require('connect');
 const timings = require('server-timings');
 
 const TATRY_PORT = process.env.TATRY_PORT || 3080;
 const TATRY_DATA_PATH = process.env.TATRY_DATA_PATH || '/var/lib/tatry';
 
-const app = express();
+const app = connect();
 
-app.enable('trust proxy');
-app.disable('x-powered-by');
-app.set('query parser', 'simple');
-
+app.use(function(req, res, next) {
+  res.locals = {};
+  next();
+});
 app.use(timings);
 
 const { v2, v1 } = require('./lib/tatry')(TATRY_DATA_PATH);
@@ -21,6 +21,7 @@ app.use('/api/v1', v1);
 module.exports = app;
 
 if (!module.parent) {
-  app.listen(TATRY_PORT);
+  const { createServer } = require('http');
+  createServer(app).listen(TATRY_PORT);
   console.log('Listening on port', TATRY_PORT);
 }

1f2686daa23489e34454e0467be70bf66ac8da75
Damian Krzeminski <pirxpilot@furkot.com>
2025-05-17 11:22:07 -0600

    use `supertest-fetch` instead of `supertest`
    it has fewer dependencies
    
    one strange thing is we have to set cache mode
    otherwise fetch sets `Cache-Control` to `no-cache` which prevents testing `ETag`
    functionality
    
    see:
    https://github.com/nodejs/undici/issues/1930

--- test/tatry.js
+++ test/tatry.js
@@ -1,97 +1,96 @@
 process.env.TATRY_DATA_PATH = `${__dirname}/fixtures/data`;
 
+const http = require('node:http');
 const test = require('node:test');
 const assert = require('node:assert/strict');
-const request = require('supertest');
+const { makeFetch } = require('supertest-fetch');
 
 const app = require('../');
+
+const request = makeFetch(http.createServer(app));
 
 test('tatry', async function (t) {
   await t.test('v1', async function (t) {
     await t.test('responds to get', async function () {
-      const response = await request(app)
-        .get('/api/v1/lookup')
-        .query({ locations: '40.483468,-106.827126|40.5,-106.1|40.8,-106.9' })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect('Content-Length', '187')
-        .expect('ETag', '"bb-om3Uf+cI6lz8xEaVNPn1nWf0fjw"')
-        .expect('Server-Timing', /total.+, lookup.+/)
-        .expect(200);
-      const {
-        body: { results }
-      } = response;
-      assert.deepEqual(results, [
-        { latitude: 40.483468, longitude: -106.827126, elevation: 2082.5 },
-        { latitude: 40.5, longitude: -106.1, elevation: 3065 },
-        { latitude: 40.8, longitude: -106.9, elevation: 2474 }
-      ]);
+      await request('/api/v1/lookup?locations=40.483468,-106.827126|40.5,-106.1|40.8,-106.9')
+        .expectHeader('Content-Type', 'application/json; charset=utf-8')
+        .expectHeader('Content-Length', '187')
+        .expectHeader('ETag', '"bb-om3Uf+cI6lz8xEaVNPn1nWf0fjw"')
+        .expectHeader('Server-Timing', /total.+, lookup.+/)
+        .expectStatus(200)
+        .expectBody({
+          results: [
+            { latitude: 40.483468, longitude: -106.827126, elevation: 2082.5 },
+            { latitude: 40.5, longitude: -106.1, elevation: 3065 },
+            { latitude: 40.8, longitude: -106.9, elevation: 2474 }
+          ]
+        });
     });
 
     await t.test('responds with None-Modified if ETag matches', async function () {
-      await request(app)
-        .get('/api/v1/lookup')
-        .query({ locations: '40.483468,-106.827126|40.5,-106.1|40.8,-106.9' })
-        .set('If-None-Match', '"bb-om3Uf+cI6lz8xEaVNPn1nWf0fjw"')
-        .set('Accept', 'application/json')
-        .expect(304);
+      await request('/api/v1/lookup?locations=40.483468,-106.827126|40.5,-106.1|40.8,-106.9', {
+        cache: 'no-cache', // HACK: fetch sets 'Cache-Control' to 'no-cache' otherwise
+        headers: {
+          'If-None-Match': '"bb-om3Uf+cI6lz8xEaVNPn1nWf0fjw"',
+          Accept: 'application/json'
+        }
+      }).expectStatus(304);
     });
 
     await t.test('responds to post', async function () {
-      const response = await request(app)
-        .post('/api/v1/lookup')
-        .send({
-          locations: [
-            { latitude: 40.483468, longitude: -106.827126 },
-            { latitude: 40.5, longitude: -106.1 },
-            { latitude: 40.8, longitude: -106.9 }
-          ]
+      const locations = [
+        { latitude: 40.483468, longitude: -106.827126 },
+        { latitude: 40.5, longitude: -106.1 },
+        { latitude: 40.8, longitude: -106.9 }
+      ];
+      await request('/api/v1/lookup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          locations
         })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200);
-      const {
-        body: { results }
-      } = response;
-
-      assert.deepEqual(results, [
-        { latitude: 40.483468, longitude: -106.827126, elevation: 2082.5 },
-        { latitude: 40.5, longitude: -106.1, elevation: 3065 },
-        { latitude: 40.8, longitude: -106.9, elevation: 2474 }
-      ]);
+      })
+        .expectHeader('Content-Type', /json/)
+        .expectStatus(200)
+        .expectBody({
+          results: [
+            { latitude: 40.483468, longitude: -106.827126, elevation: 2082.5 },
+            { latitude: 40.5, longitude: -106.1, elevation: 3065 },
+            { latitude: 40.8, longitude: -106.9, elevation: 2474 }
+          ]
+        });
     });
 
     await t.test('responds to get with invalid coordinates', async function () {
-      const response = await request(app)
-        .get('/api/v1/lookup')
-        .query({ locations: '10,10' })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200);
-      const {
-        body: { results }
-      } = response;
-
-      assert.deepEqual(results, [{ longitude: 10, latitude: 10, elevation: -32768 }]);
+      const response = await request('/api/v1/lookup?locations=10,10', {
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+        .expectHeader('Content-Type', /json/)
+        .expectStatus(200)
+        .json();
+      assert.deepEqual(response.results, [{ longitude: 10, latitude: 10, elevation: -32768 }]);
     });
   });
 
   await t.test('v2', async function (t) {
     await t.test('responds to get', async function () {
-      const response = await request(app)
-        .get('/api/v2/lookup')
-        .query({ lls: '-106.827126,40.483468|-106.1,40.5|-106.9,40.8' })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect('Content-Length', '139')
-        .expect('ETag', '"8b-7qj+jGWXNhf1LTbAVypElfOgubc"')
-        .expect('Server-Timing', /total.+, lookup.+/)
-        .expect(200);
-      const {
-        body: { results }
-      } = response;
-
-      assert.deepEqual(results, [
+      const response = await request('/api/v2/lookup?lls=-106.827126,40.483468|-106.1,40.5|-106.9,40.8', {
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+        .expectHeader('Content-Type', 'application/json; charset=utf-8')
+        .expectHeader('Content-Length', '139')
+        .expectHeader('ETag', '"8b-7qj+jGWXNhf1LTbAVypElfOgubc"')
+        .expectHeader('Server-Timing', /total.+, lookup.+/)
+        .expectStatus(200)
+        .json();
+      assert.deepEqual(response.results, [
         { ll: [-106.827126, 40.483468], elevation: 2082.5 },
         { ll: [-106.1, 40.5], elevation: 3065 },
         { ll: [-106.9, 40.8], elevation: 2474 }
@@ -99,32 +98,34 @@ test('tatry', async function (t) {
     });
 
     await t.test('responds with None-Modified if ETag matches', async function () {
-      await request(app)
-        .get('/api/v2/lookup')
-        .query({ lls: '-106.827126,40.483468|-106.1,40.5|-106.9,40.8' })
-        .set('If-None-Match', '"8b-7qj+jGWXNhf1LTbAVypElfOgubc"')
-        .set('Accept', 'application/json')
-        .expect(304);
+      await request('/api/v2/lookup?lls=-106.827126,40.483468|-106.1,40.5|-106.9,40.8', {
+        cache: 'no-cache', // HACK: fetch sets 'Cache-Control' to 'no-cache' otherwise
+        headers: {
+          'If-None-Match': '"8b-7qj+jGWXNhf1LTbAVypElfOgubc"',
+          Accept: 'application/json'
+        }
+      }).expectStatus(304);
     });
 
     await t.test('responds to post', async function () {
-      const response = await request(app)
-        .post('/api/v2/lookup')
-        .send({
+      const response = await request('/api/v2/lookup', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
           lls: [
             [-106.827126, 40.483468],
             [-106.1, 40.5],
             [-106.9, 40.8]
           ]
         })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200);
-      const {
-        body: { results }
-      } = response;
-
-      assert.deepEqual(results, [
+      })
+        .expectHeader('Content-Type', /json/)
+        .expectStatus(200)
+        .json();
+      assert.deepEqual(response.results, [
         { ll: [-106.827126, 40.483468], elevation: 2082.5 },
         { ll: [-106.1, 40.5], elevation: 3065 },
         { ll: [-106.9, 40.8], elevation: 2474 }
@@ -132,17 +133,15 @@ test('tatry', async function (t) {
     });
 
     await t.test('responds to get with invalid coordinates', async function () {
-      const response = await request(app)
-        .get('/api/v2/lookup')
-        .query({ lls: '10,10' })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200);
-      const {
-        body: { results }
-      } = response;
-
-      assert.deepEqual(results, [
+      const response = await request('/api/v2/lookup?lls=10,10', {
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+        .expectHeader('Content-Type', /json/)
+        .expectStatus(200)
+        .json();
+      assert.deepEqual(response.results, [
         {
           ll: [10, 10]
         }
